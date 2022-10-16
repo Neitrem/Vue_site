41a214f210de4548253798f5fe25254f0d170e76
Neitrem <a.v.gorshenin000@gmail.com>
2022-10-16 15:39:54 +0300

    2

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,99 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="app">
+        <top-panel></top-panel>
+        <div class="wrap">
+            <div class="left-panel">
+                <div class="nav">
+                    <div class="nav__container">
+                        <ul class="list">
+                            <li class="list-el">Главная</li>
+                            <li class="list-el">Авто</li>
+                            <li class="list-el">Недвижимость</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="line"></div>
+            <div class="rigth-panel">
+                
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopPanel from '@/components/TopPanel'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+        TopPanel
+    },
+    data () {
+        return {
+
+        }
+    }, 
+    methods: {
+
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-family: 'Courier New', Courier, monospace
 }
+
+
+.post {
+    margin-top: 15px;
+    padding: 15px;
+    border: 1px solid teal;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+.input {
+    padding: 10px 15px;
+    margin-top: 15px;
+    width: 100%;
+    border: 1px solid teal;
+}
+
+.btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+}
+
+.wrap {
+    height: 1000px;
+    margin: 15px;
+    display: flex;
+    justify-content: space-between;
+}
+.left-panel {
+    width: 20%;
+    height: 100%;
+}
+
+.rigth-panel {
+    width: 60%;
+    height: 100%;
+}
+
+.line {
+    margin: 15px 0 15px 0;
+    border-left: 1px solid gray;
+    height: 100%;
+}
+
 </style>
---
+++ src/components/TopPanel.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="top">
+    <div class="top__container">
+        <p class="logo">Brom.ru</p>
+        <p class="second-name">Доска объявлений авто и недвижимости</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.top__container {
+    margin: 15px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    border-bottom: 1px solid gray;
+}
+
+.second-name {
+    text-align: center;
+    display: inline-block;
+    font-size: 20px;
+    font-weight: 500;
+}
+
+.logo {
+    display: inline-block;
+    width: fit-content;
+
+    font-size: 70px;
+    font-weight: 600;
+    color: transparent;
+    background-clip: text;
+    background-image: linear-gradient(45deg, #553c9a, #ee4b2b);
+}
+</style>
