b245b27d391eabf8cd6742a3f1365e7ae88113c4
Neitrem <a.v.gorshenin003@gmail.com>
2022-11-02 12:51:10 +0300

    123\

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     devServer: {
-          proxy: 'http://localhost/items',
+          proxy: 'http://localhost:3000/items',
       },
     transpileDependencies: true
 })
